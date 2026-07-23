99153db1050ae0642153e3ebf3b6a038ee683d7f
Shaik Moukhil <shaikmoukhil@gmail.com>
2026-07-23 18:10:48 +0530

    Update order authentication middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const authMiddleware = (req, res, next) => {
+    console.log("AUTH MIDDLEWARE HIT");
     try {
         const authHeader = req.headers.authorization;
 
