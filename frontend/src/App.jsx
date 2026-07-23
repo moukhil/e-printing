b295c045910734cd92dc6a7dd05606b087dec9b1
Shaik Moukhil <shaikmoukhil@gmail.com>
2026-07-23 15:47:06 +0530

    changed home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import './App.css'
 import { Routes, Route } from "react-router-dom"
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Home from './pages/home'
+import Home from './pages/Home'
 import Orders from "./pages/Orders"
 import Shop from "./pages/Shop"
 import Profile from './pages/Profile'
