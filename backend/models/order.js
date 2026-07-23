8eb2b0283ed167cbfd1b21284ba05324a4ef4b5e
Shaik Moukhil <shaikmoukhil@gmail.com>
2026-07-23 17:54:37 +0530

    Add user reference to orders

--- backend/models/order.js
+++ backend/models/order.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
+
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+
   items: [
     {
       fileUrl: String,
@@ -11,8 +18,17 @@ const orderSchema = new mongoose.Schema({
       price: Number,
     }
   ],
-  totalPrice: { type: Number, required: true },
-  createdAt: { type: Date, default: Date.now }
+
+  totalPrice: {
+    type: Number,
+    required: true
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+
 });
 
 module.exports = mongoose.model('Order', orderSchema);
